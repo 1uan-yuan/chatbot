deb10dd725be909eff4104f40d3376dff7a66c1d
1yuan-yuan <cao371@purdue.edu>
2023-07-17 16:54:56 +0800

    Basic Functions

---
+++ bot.py
@@ -0,0 +1,27 @@
+import gradio as gr
+import openai
+
+openai.api_key = "YOUR_API_KEY"
+
+message_history = []
+
+def predict():
+    global message_history
+    message_history.append({"rold":"User", "content":input})
+    completion = openai.Completion.create(
+        model = "davinci",
+        messages = message_history
+    )
+    reply_content = completion.choices[0].message.content
+    message_history.append({"role":"assistant", "content":reply_content})
+    response = [(message_history[i]["content"], message_history[i + 1]["content"]) for i in range(len(message_history) - 1, 2)]
+    return response
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        txt = gr.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
+        txt.submit(predict, txt, chatbot)
+        # txt.submit(lambda: "", None, txt)
+        txt.submit(None, None, txt, _js="() => ('')")
+
+    demo.launch()
